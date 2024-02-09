06472e868294df88fac5fd3395defbfc596c7048
albamartinmz <albamartinsonido@gmail.com>
2024-02-09 15:25:48 +0100

    try add background page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Nav from '../components/Nav/Nav.jsx';
 import Footer from '../components/Footer/Footer.jsx';
+import BackgroundHome from '../assets/images/Background.jpg';
 
 const Home = () => {
   return (
     <>
       <Nav/>
-      <h1>GALERÍA</h1>
+      <div className="container">
+        <image src= {BackgroundHome} alt="Fondo simulando pared de ladrillos blancos"/>
+        <h1>GALERÍA</h1>
+      </div>
       <Footer/>
     </>
   )
