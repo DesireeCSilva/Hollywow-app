e85d2c4f1174aaecf06289edff80fb5745fdc56c
albamartinmz <albamartinsonido@gmail.com>
2024-02-13 14:59:04 +0100

    indentation changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,48 +2,48 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PosterCard = styled.div`
-  background-color: black;
-  border-radius: 10px;
-  padding: 50px;
-  margin: 50px;
-  margin-right: 500px;
-  margin-left: 500px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    background-color: black;
+    border-radius: 10px;
+    padding: 50px;
+    margin: 50px;
+    margin-right: 500px;
+    margin-left: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 const PosterImage = styled.img`
-  max-width: 50%;
-  height: auto;
-  object-fit: cover;
-  border-radius: 5px;
-  margin-bottom: 10px;
+    max-width: 50%;
+    height: auto;
+    object-fit: cover;
+    border-radius: 5px;
+    margin-bottom: 10px;
 `;
 
 const PosterInfo = styled.div`
-  color: white;
-  text-align: center;
+    color: white;
+    text-align: center;
 `;
 
 const ButtonDelete = styled.button`
-  background-color: #9C325C;
-  border-color: #CEA436;
-  border-radius: 6px;
-  color:#CEA436;
-  width: 91px;
-  height: 28px;
-  margin-bottom: 10%;
-  `;
+    background-color: #9C325C;
+    border-color: #CEA436;
+    border-radius: 6px;
+    color:#CEA436;
+    width: 91px;
+    height: 28px;
+    margin-bottom: 10%;
+`;
 
-  const ButtonEdit = styled.button`
-  background-color: #CEA436; 
-  border-color: #9C325C;
-  border-radius: 6px;
-  color:#9C325C;
-  width: 91px;
-  height: 28px;
-  `;
+const ButtonEdit = styled.button`
+    background-color: #CEA436; 
+    border-color: #9C325C;
+    border-radius: 6px;
+    color:#9C325C;
+    width: 91px;
+    height: 28px;
+`;
 
 const Card = ({ posters }) => {
     // const { imageUrl, name, director, year } = poster;
@@ -52,22 +52,21 @@ const Card = ({ posters }) => {
     return ( 
 
     <>
-      {posters.map(poster => { return ( 
-      <>
-      <PosterCard key={poster.id} className="poster-card">
-        <PosterImage src={poster.imageUrl} alt={poster.name} className="poster-card__image" />
-        <PosterInfo className="poster-card__info">
-          <h2 className="poster-card__name">{poster.name}</h2>
-          <p className="poster-card__director">Director: {poster.director}</p>
-          <p className="poster-card__year">Year: {poster.year}</p>
-        </PosterInfo>
-        <ButtonDelete className="button-delete">DELETE</ButtonDelete>
-        <ButtonEdit className="button-edit">EDIT</ButtonEdit>
-      </PosterCard>
-      </>
-    );
-    })}
-   
+        {posters.map(poster => { return ( 
+        <>
+            <PosterCard key={poster.id} className="poster-card">
+                <PosterImage src={poster.imageUrl} alt={poster.name} className="poster-card__image" />
+                <PosterInfo className="poster-card__info">
+                    <h2 className="poster-card__name">{poster.name}</h2>
+                    <p className="poster-card__director">Director: {poster.director}</p>
+                    <p className="poster-card__year">Year: {poster.year}</p>
+                </PosterInfo>
+                <ButtonDelete className="button-delete">DELETE</ButtonDelete>
+                <ButtonEdit className="button-edit">EDIT</ButtonEdit>
+            </PosterCard>
+        </>
+        );
+        })}
     </>
     );
 };
