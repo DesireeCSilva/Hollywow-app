517a5ad61870fb0ac4adb14a94ccd4f72a7f3a6f
rebecavm28 <rebe14@hotmail.es>
2024-02-15 14:45:09 +0100

    cartelprincipal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,8 +8,7 @@ const HomePage = () => {
     return (
     <>
     <div className="main-container">
-        <img src='src\assets\images\fotopelis.jpg' className="fotopelis"/>
-        <img src='src\assets\images\carterela.png' className="cartel"/>
+        <img src='src\assets\images\cartelprincipal.png' className="fotopelis"/>
         <h1 className="title">GALERÍA</h1>
         <section>
         <Card posters={posters}/>
