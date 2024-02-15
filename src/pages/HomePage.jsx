e42a57b3455d670408b7f1e13e7acb2b62555630
rebecavm28 <rebe14@hotmail.es>
2024-02-15 13:33:00 +0100

    cartel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,6 +11,7 @@ const HomePage = () => {
     {/* {<Nav/>} */}
     <div className="main-container">
         <img src='src\assets\images\fotopelis.jpg' className="fotopelis"/>
+        <img src='src\assets\images\carterela.png' className="cartel"/>
         <h1 className="title">GALERÍA</h1>
         <section>
         <Card posters={posters}/>
