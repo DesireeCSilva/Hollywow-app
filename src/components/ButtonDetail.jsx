a201ffbfca099258bfdd8340228dfa20063d9290
Andreamartinn17 <andreamartinn17@gmail.com>
2024-02-28 10:06:01 +0100

    buttonDetail comments

--- src/components/ButtonDetail.jsx
+++ src/components/ButtonDetail.jsx
@@ -1,3 +1,5 @@
+//ButtonDetail de la Card que redirige a la página de CardDetail (página de detalle de un póster).
+
 import React from 'react'
 import '../components/ButtonDetail.css'
 import { Link } from 'react-router-dom'
