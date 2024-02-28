d23136c4e6fe779a5a0fcee67efd6003ee8ab59c
Andreamartinn17 <andreamartinn17@gmail.com>
2024-02-28 10:06:59 +0100

    buttonEdit comments

--- src/components/ButtonEdit.jsx
+++ src/components/ButtonEdit.jsx
@@ -1,3 +1,5 @@
+//ButtonEdit de la CardDetail que redirige a la página de UpdateForm (formulario de editar póster).
+
 import React from 'react';
 import '../components/ButtonEdit.css'
 import { Link } from 'react-router-dom';
