cb2b88e34b8f3e625fe9c3d046cb7e3164d62aa2
Andreamartinn17 <andreamartinn17@gmail.com>
2024-02-28 12:06:39 +0100

    CreateForm comments

--- src/pages/CreateForm.jsx
+++ src/pages/CreateForm.jsx
@@ -4,11 +4,13 @@ import { createPoster } from '../services/posterServices';
 import { useNavigate } from 'react-router-dom';
 
 const CreateForm = () => {
-    const { register, formState: { errors }, handleSubmit, reset } = useForm();
-    const navigate = useNavigate()
-    const onSubmit = async (newPoster) => {
-        await createPoster(newPoster) 
-        navigate("/")
+    const { register, formState: { errors }, handleSubmit, reset } = useForm(); // Utiliza el hook useForm para manejar el estado del formulario
+    const navigate = useNavigate() // Hook para redirigir la navegación a una página concreta.
+    const onSubmit = async (newPoster) => { // Define una función asincrónica onSubmit para manejar el envío del formulario.
+        await createPoster(newPoster) // Petición de espera a la función createPoster para crear un nuevo póster.
+        navigate("/") //Uso del hook UseNavigate para que la función CreateForm nos redirija a la Home.
+
+        //INTENTO DE VALIDACIÓN NO CONSEGUIDO:
         // console.log(response);
         // if ( response.ok ) {
         //     alert('¡Formulario enviado con éxito!');
@@ -24,7 +26,8 @@ const CreateForm = () => {
             <div>
                 <img src="../src/assets/images/Añade-pelicula.png" className='Add'></img> 
             </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            {/* // Inicia el formulario y llama a la función handleSubmit para manejar el envío del formulario. */}
+            <form onSubmit={handleSubmit(onSubmit)}> 
                 <div>
                     <label className='image'>Imagen</label>
                     <input type="url" {...register('imageUrl', { 
